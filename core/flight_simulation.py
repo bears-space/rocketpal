18e6aeeb642d334de428c7d0441b90cad29bc0a3
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2024-01-08 18:02:43 +0100

    Add dummy code

---
+++ core/flight_simulation.py
@@ -0,0 +1,3 @@
+class FlightSimulation:
+    def __init__(self) -> None:
+        pass
--- stargaze-flight-simulation.py
+++ stargaze-flight-simulation.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
-def main():
-    # TODO
-    pass
 
+from core.flight_simulation import FlightSimulation
+
+
+def main() -> None:
+    # Initialize flight simulation
+    sim: FlightSimulation = FlightSimulation()
+
+    # TODO Load flight parameters
+
+    # TODO Run simulation
+
+    # TODO Save results
+
+
+# Run main if launched directly
 if __name__ == "__main__":
     main()
