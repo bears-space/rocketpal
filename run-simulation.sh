cecd56bdb7c630bad3a8172607dae036fda811ff
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2024-11-26 16:52:25 +0100

    Use underscores for stargaze_flight_simulation.py

--- run-simulation.sh
+++ run-simulation.sh
@@ -4,4 +4,4 @@
 mkdir -p ./output
 
 # Run simulation and redirect terminal output to ./output/log.txt
-python stargaze-flight-simulation.py ./input --output ./output &> ./output/log.txt
+python stargaze_flight_simulation.py ./input --output ./output &> ./output/log.txt
