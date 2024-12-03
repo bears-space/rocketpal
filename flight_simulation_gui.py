7c3349d81068d438eab41fbe5c8a9ba63361a631
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2024-12-03 17:26:43 +0100

    Use Qt for GUI instead of tkinter

--- flight_simulation_gui.py
+++ flight_simulation_gui.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
+# type: ignore
 
 import logging
 
-from tkinter import Tk
-from tkinter.ttk import Frame, Label
+import sys
+import random
+from PySide6 import QtCore, QtWidgets
+
+
+class HelloWorldWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World!", alignment=QtCore.Qt.AlignCenter)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        self.text.setText(f"Hello World! Random number is {random.randint(0, 1000)}.")
 
 
 if __name__ == "__main__":
@@ -11,15 +29,14 @@ if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.INFO)
 
-    # Prepare main window
-    root = Tk()
-    root.title("FSG - Flight Simulation GUI")
-    root.geometry("1280x720")
-    frame = Frame(root, padding=10)
-    frame.grid()
+    # Prepare application
+    app = QtWidgets.QApplication([])
+    app.setApplicationName("FSG - Flight Simulation GUI")
 
     # Hello world test
-    a = Label(frame, text="Hello world!").grid(column=0, row=0)
+    widget = HelloWorldWidget()
+    widget.resize(1280, 720)
+    widget.show()
 
     # Enter GUI main loop
-    root.mainloop()
+    sys.exit(app.exec())
