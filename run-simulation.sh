c65c83d49799961a315285a8789c7577338a7e8c
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2025-01-10 17:15:34 +0100

    Move config files from input to template
    This is preparation for the GUI

--- run-simulation.sh
+++ run-simulation.sh
@@ -4,4 +4,4 @@
 mkdir -p ./output
 
 # Run simulation and redirect terminal output to ./output/log.txt
-python simulation/simulation.py ./input --output ./output &> ./output/log.txt
+python simulation/simulation.py ./template --output ./output &> ./output/log.txt
