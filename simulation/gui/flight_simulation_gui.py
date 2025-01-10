d534db0a6b25db7fd89a9d8fd169823da33e5a67
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2025-01-10 17:03:24 +0100

    Implement simulation selection

--- simulation/gui/flight_simulation_gui.py
+++ simulation/gui/flight_simulation_gui.py
@@ -1,5 +1,6 @@
 # type: ignore
 
+import platformdirs
 from pathlib import Path
 import sys
 import os
@@ -7,11 +8,15 @@ from PySide6 import QtCore, QtWidgets
 
 from simulation import load_configs_and_run_simulation
 
-INPUT_FOLDER = "./input"
+SIMULATION_CONFIG_BASE_FOLDER = (
+    platformdirs.user_config_dir() + "/bears-flight-simulation/simulations"
+)
 OUTPUT_FOLDER = str(Path(os.path.realpath(sys.argv[0])).parent) + "/output"
 
 
 class FlightSimulationGUI(QtWidgets.QWidget):
+    current_selection: str | None = None
+
     def __init__(self):
         super().__init__()
 
@@ -32,11 +37,19 @@ class FlightSimulationGUI(QtWidgets.QWidget):
 
         self.selection_layout_widget.setLayout(_selection_layout)
         self.simulation_selector_dropdown = QtWidgets.QComboBox()
+        self.simulation_selector_dropdown.currentTextChanged.connect(
+            self._simulation_selector_dropdown_changed
+        )
+        self._refresh_selectable_simulations()
         _selection_layout.addWidget(self.simulation_selector_dropdown)
 
         self.selection_edit_button = QtWidgets.QPushButton("Edit")
         self.selection_edit_button.clicked.connect(self._selection_edit_button_pressed)
         _selection_layout.addWidget(self.selection_edit_button)
+
+        self.selection_new_button = QtWidgets.QPushButton("New")
+        self.selection_new_button.clicked.connect(self._selection_new_button_pressed)
+        _selection_layout.addWidget(self.selection_new_button)
 
         self.run_simulation_button = QtWidgets.QPushButton("Run Simulation")
         self.run_simulation_button.clicked.connect(self._run_simulation_button_pressed)
@@ -54,13 +67,42 @@ class FlightSimulationGUI(QtWidgets.QWidget):
         )
         self.layout.addSpacerItem(_spacer_bottom)
 
+    def _simulation_selector_dropdown_changed(self, new_text):
+        self.current_selection = new_text
+
     def _selection_edit_button_pressed(self):
+        # TODO Launch config editor for the currently selected simulation
         pass
+
+    def _selection_new_button_pressed(self):
+        self._refresh_selectable_simulations()
 
     def _run_simulation_button_pressed(self):
         load_configs_and_run_simulation(
-            config_folder=INPUT_FOLDER, output_folder=OUTPUT_FOLDER
+            config_folder=SIMULATION_CONFIG_BASE_FOLDER + "/" + self.current_selection,
+            output_folder=OUTPUT_FOLDER,
         )
 
     def _show_results_button_pressed(self):
         pass
+
+    def _refresh_selectable_simulations(self):
+        old_selection = self.simulation_selector_dropdown.currentText()
+
+        self.simulation_selector_dropdown.clear()
+        self.simulation_selector_dropdown.addItems(get_config_folders())
+
+        index_equal_to_old_selection = self.simulation_selector_dropdown.findText(
+            old_selection
+        )
+        if index_equal_to_old_selection >= 0:
+            self.simulation_selector_dropdown.setCurrentIndex(
+                index_equal_to_old_selection
+            )
+
+
+def get_config_folders() -> set[str]:
+    # Create the simulation config folder if it doesn't exist yet
+    Path(SIMULATION_CONFIG_BASE_FOLDER).mkdir(parents=True, exist_ok=True)
+
+    return os.listdir(SIMULATION_CONFIG_BASE_FOLDER)
