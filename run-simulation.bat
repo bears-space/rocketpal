46b51d85f26c5ce4d757e2068d67afd64cedce4a
Eric Ruebhagen <e.ruebhagen@tu-berlin.de>
2025-04-30 22:08:59 +0200

    Update .bat file to test

--- run-simulation.bat
+++ run-simulation.bat
@@ -1,7 +1,7 @@
 @echo off
 
-# Ensure the output folder exists
+: Ensure the output folder exists
 if not exist %cd%\output mkdir %cd%\output
 
-# Run simulation and redirect terminal output to ./output/log.txt
+: Run simulation and redirect terminal output to ./output/log.txt
 python bears_flight_simulation\simulation.py .\template --output %cd%\output >> %cd%\output\log.txt 2>&1
