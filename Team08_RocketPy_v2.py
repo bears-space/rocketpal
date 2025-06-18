16c0be429ab26bede9e698e07daa18cc9df563e0
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2025-06-18 13:03:20 +0200

    version bump for EuRoC single-file launcher

--- Team08_RocketPy_v2.py
+++ Team08_RocketPy_v2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# NOTE for EURoC evaluators: Please make sure to install the Python requirements listed in the requirements.txt before running
+# NOTE for EuRoC evaluators: Please make sure to install the Python requirements listed in the requirements.txt before running
 
 import logging
 from pathlib import Path
