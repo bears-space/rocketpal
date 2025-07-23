0c08c1f92b1c70dad5020ff9e1100721bcc090fe
niklas.dg.freund <niklas.dg.freund@campus.tu-berlin.de>
2025-07-23 12:35:25 +0200

    test: fix test_airbrake_controllers.py tests

--- tests/utilities/test_airbrake_controllers.py
+++ tests/utilities/test_airbrake_controllers.py
@@ -2,7 +2,7 @@ import pytest
 
 import math
 from pathlib import Path
-from rocketpy import Environment, AirBrakes
+from rocketpy import Environment, AirBrakes, Rocket
 
 from bears_flight_simulation.utilities.airbrake_controllers import (
     State,
@@ -11,6 +11,12 @@ from bears_flight_simulation.utilities.airbrake_controllers import (
 
 ZERO_DRAG_CURVE_PATH = (
     Path(__file__).parent.parent / "__assets__" / "airbrake_zero_drag_curve.csv"
+)
+ROCKET_POWER_OFF_DRAG_CURVE_PATH = (
+    Path(__file__).parent.parent / "__assets__" / "stargaze_power_off_drag_curve.csv"
+)
+ROCKET_POWER_ON_DRAG_CURVE_PATH = (
+    Path(__file__).parent.parent / "__assets__" / "stargaze_power_on_drag_curve.csv"
 )
 REFERENCE_AREA = math.pi * ((0.1236 / 2.0) ** 2.0)  # A = πr²
 
@@ -21,6 +27,18 @@ def standard_airbrakes() -> AirBrakes:
         drag_coefficient_curve=str(ZERO_DRAG_CURVE_PATH),
         reference_area=REFERENCE_AREA,
         clamp=True,
+    )
+
+
+@pytest.fixture
+def standard_rocket() -> Rocket:
+    return Rocket(
+        radius=0.1236,
+        mass=18.38121,
+        inertia=(6.321, 6.321, 0.034),
+        power_off_drag=str(ROCKET_POWER_OFF_DRAG_CURVE_PATH),
+        power_on_drag=str(ROCKET_POWER_OFF_DRAG_CURVE_PATH),
+        center_of_mass_without_motor=0,
     )
 
 
@@ -87,9 +105,12 @@ def test_state__history_format():
     assert state.w_z == W_Z
 
 
-def test_stupid_full_extension_controller__pad_no_history(standard_airbrakes):
+def test_stupid_full_extension_controller__pad_no_history(
+    standard_airbrakes, standard_rocket
+):
     _, deployment_level, _ = stupid_full_extension_controller(
         env=Environment(),
+        rocket=standard_rocket,
         time=2.0,
         sampling_rate=10.0,
         state_raw=[
@@ -115,9 +136,12 @@ def test_stupid_full_extension_controller__pad_no_history(standard_airbrakes):
     assert deployment_level == 0.0
 
 
-def test_stupid_full_extension_controller__pad_with_history(standard_airbrakes):
+def test_stupid_full_extension_controller__pad_with_history(
+    standard_airbrakes, standard_rocket
+):
     _, deployment_level, _ = stupid_full_extension_controller(
         env=Environment(),
+        rocket=standard_rocket,
         time=2.0,
         sampling_rate=10.0,
         state_raw=[
@@ -143,9 +167,12 @@ def test_stupid_full_extension_controller__pad_with_history(standard_airbrakes):
     assert deployment_level == 0.0
 
 
-def test_stupid_full_extension_controller__ascent_before_treshold(standard_airbrakes):
+def test_stupid_full_extension_controller__ascent_before_treshold(
+    standard_airbrakes, standard_rocket
+):
     _, deployment_level, _ = stupid_full_extension_controller(
         env=Environment(),
+        rocket=standard_rocket,
         time=2.0,
         sampling_rate=10.0,
         state_raw=[
@@ -173,9 +200,12 @@ def test_stupid_full_extension_controller__ascent_before_treshold(standard_airbr
     assert deployment_level == 0.0
 
 
-def test_stupid_full_extension_controller__ascent_after_treshold(standard_airbrakes):
+def test_stupid_full_extension_controller__ascent_after_treshold(
+    standard_airbrakes, standard_rocket
+):
     _, deployment_level, _ = stupid_full_extension_controller(
         env=Environment(),
+        rocket=standard_rocket,
         time=2.0,
         sampling_rate=10.0,
         state_raw=[
@@ -203,9 +233,12 @@ def test_stupid_full_extension_controller__ascent_after_treshold(standard_airbra
     assert deployment_level == 1.0
 
 
-def test_stupid_full_extension_controller__after_apogee(standard_airbrakes):
+def test_stupid_full_extension_controller__after_apogee(
+    standard_airbrakes, standard_rocket
+):
     _, deployment_level, _ = stupid_full_extension_controller(
         env=Environment(),
+        rocket=standard_rocket,
         time=30.0,
         sampling_rate=10.0,
         state_raw=[
