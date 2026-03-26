b00d83b5cfae4a7daff5029426f7722fdf8244a4
Niklas Freund <nalquas.dev@gmail.com>
2026-03-26 17:02:44 +0100

    Remove unmaintained run-simulation.bat

--- run-simulation.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-: Ensure the output folder exists
-if not exist %cd%\output mkdir %cd%\output
-
-: Run simulation and redirect terminal output to ./output/log.txt
-uv run python -m bears_flight_simulation sim .\template --output %cd%\output >> %cd%\output\log.txt 2>&1
